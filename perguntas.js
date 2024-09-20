9925106840c32d8c6fb938c83df3d5d80848d729
LaraSop <162444048+LaraSop@users.noreply.github.com>
2024-09-20 11:46:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Livros',
+    'O que são os livros para você?',
+    'Os livros são mais que apenas textos encadernados, são um refúgio do mundo real. Onde a pessoa pode ir quando a vida está muito pesada.'
+)
+
+criaCartao(
+    'Autores',
+    'Se você pudesse escolher um universo de algum autor para viver, qual autor escolheria?',
+    'Ali Hazelwood'
+)
+
+criaCartao(
+    'Livro',
+    'Tem um livro favorito?',
+    'Acho meio impossivel ter um livro favorito mas se pudesse escolher acho que seria a saga Estilhaça-me'
+)
+
+criaCartao(
+     'Gênero',
+    'Qual seu gênero(s) favorito(s)?',
+    'Suspense, distopia e Ficção/Fantasia'
+)
